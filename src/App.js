d37f185d5cb4107b422fc4855b744faa0111001b
Joe Collins <JoeCollins@Charles-7.local>
2019-09-18 12:04:17 +0800

    i dont know what is happening

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>All about the root word "yeet"</h1>
+      <h2>history:</h2>
+      <img alt="yeet guy" src="https://www.dailydot.com/wp-content/uploads/2018/09/yeet.jpg" width="500px" heiht="500px"></img>
+      <img alt="yeezus" src="https://upload.wikimedia.org/wikipedia/en/0/03/Yeezus_album_cover.png" width="500px" height="270px"></img>
+      <h2>conjunctions:</h2>
+      <table>
+        <tr>
+          <td>pronoun</td>
+          <td>conjunction</td>
+        </tr>
+        <tr>
+          <td>I</td>
+          <td>Yeet</td>
+        </tr>
+        <tr>
+          <td>You</td>
+          <td>Yeet</td>
+        </tr>
+        <tr>
+          <td>he/she/it</td>
+          <td>yeets</td>
+        </tr>
+        <tr>
+          <td>I</td>
+          <td>yoted</td>
+        </tr>
+        <tr>
+          <td>You</td>
+          <td>yoted</td>
+        </tr>
+        <tr>
+          <td>he/she/it</td>
+          <td>yoted</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
